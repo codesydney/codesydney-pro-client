08420af1643591e5cc56fb45ea3abdc808a77b46
Promie Yutasane <promie.yutasane@gmail.com>
2024-06-29 06:32:48 +1000

    CSP-19 finalising

--- src/components/nav/Navbar.tsx
+++ src/components/nav/Navbar.tsx
@@ -23,25 +23,25 @@ const Navbar: FC = () => {
         <div
           className={`nav-links duration-500 md:static absolute bg-white md:min-h-fit min-h-[30vh] left-0 ${menuOpen ? 'opacity-100 visible top-[130%]' : 'opacity-0 invisible top-[130%]'} md:opacity-100 md:visible md:top-auto md:flex md:items-center md:px-0 z-40 transition-all`}
         >
-          <ul className="flex md:flex-row flex-col md:items-center md:gap-[4vw] gap-8">
+          <ul className="flex md:flex-row flex-col md:items-center md:gap-[4vw] gap-8 md:p-0 p-[5px]">
             <li className="hover:text-gray-500 cursor-pointer">Home</li>
             <li className="hover:text-gray-500 cursor-pointer">Developers</li>
             <li className="hover:text-gray-500 cursor-pointer">Contact Us</li>
             <li className="md:hidden block hover:text-gray-500 cursor-pointer">
-              Register
+              Login
             </li>
             <li className="md:hidden block hover:text-gray-500 cursor-pointer">
-              Login
+              Register
             </li>
           </ul>
         </div>
 
         <div className="flex items-center gap-6">
           <button className="bg-[#355878] text-white px-5 py-2 rounded-full hover:bg-[#87acec] hidden md:block">
-            Register
+            Login
           </button>
           <button className="bg-[#355878] text-white px-5 py-2 rounded-full hover:bg-[#87acec] hidden md:block">
-            Login
+            Register
           </button>
           <div
             className="text-3xl cursor-pointer md:hidden"
