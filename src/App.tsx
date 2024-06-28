47fb12e8cee7a0670af8c968c9b6fe7e08529e40
Promie Yutasane <promie.yutasane@gmail.com>
2024-06-29 09:41:56 +1000

    CSP-21 finalise footer component

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,12 @@
-import { NavLink } from 'react-router-dom'
-import { LogoHeader } from './components/LogoHeader'
 import Navbar from './components/nav/Navbar.tsx'
 import Footer from './components/Footer.tsx'
 
 export default function App() {
   return (
-    <>
+    <div className="flex flex-col min-h-screen">
       <Navbar />
-      <section className=" h-screen flex flex-col gap-10 justify-center items-center py-20">
-        <LogoHeader title="Techies4Good" spinLogo={true} />
-        <div className=" flex gap-4">
-          <NavLink
-            to={'/login'}
-            className="rounded bg-white px-4 py-2 text-black shadow-md uppercase border-2 border-black"
-          >
-            Login
-          </NavLink>
-
-          <NavLink
-            to={'/register'}
-            className="rounded bg-black px-4 py-2 text-white shadow-md uppercase border-2 border-black"
-          >
-            Register
-          </NavLink>
-        </div>
-      </section>
+      <div className="flex-1 mt-[105px]">Hello world</div>
       <Footer />
-    </>
+    </div>
   )
 }
