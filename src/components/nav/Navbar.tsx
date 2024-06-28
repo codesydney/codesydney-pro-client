459a4f2ee9dcb004cfb53064d9b80c83ff657f3e
Promie Yutasane <promie.yutasane@gmail.com>
2024-06-29 05:59:13 +1000

    CSP-19 added separator

--- src/components/nav/Navbar.tsx
+++ src/components/nav/Navbar.tsx
@@ -10,37 +10,48 @@ const Navbar: FC = () => {
   }
 
   return (
-    <nav className="flex justify-between items-center w-[92%] mx-auto p-[10px]">
-      <div>
-        <img
-          className="w-16 cursor-pointer"
-          src={logoImage}
-          alt="Code Sydney Logo"
-        />
-      </div>
+    <div className="border-b-2">
+      <nav className="flex justify-between items-center w-[92%] mx-auto p-[20px]">
+        <div>
+          <img
+            className="w-16 cursor-pointer"
+            src={logoImage}
+            alt="Code Sydney Logo"
+          />
+        </div>
 
-      <div
-        className={`nav-links duration-500 md:static absolute bg-white md:min-h-fit min-h-[60vh] left-0 ${menuOpen ? 'top-[9%]' : 'top-[-100%]'} md:w-auto w-full flex items-center px-5`}
-      >
-        <ul className="flex md:flex-row flex-col md:items-center md:gap-[4vw] gap-8">
-          <li className="hover:text-gray-500">Home</li>
-          <li className="hover:text-gray-500">Developers</li>
-          <li className="hover:text-gray-500">Contact Us</li>
-        </ul>
-      </div>
+        <div
+          className={`nav-links duration-500 md:static absolute bg-white md:min-h-fit min-h-[60vh] left-0 ${menuOpen ? 'top-[9%]' : 'top-[-100%]'} md:w-auto w-full flex items-center px-5`}
+        >
+          <ul className="flex md:flex-row flex-col md:items-center md:gap-[4vw] gap-8">
+            <li className="hover:text-gray-500">Home</li>
+            <li className="hover:text-gray-500">Developers</li>
+            <li className="hover:text-gray-500">Contact Us</li>
+            <li className="md:hidden block">
+              <button className="bg-[#a6c1ee] text-white px-5 py-2 rounded-full hover:bg-[#87acec]">
+                Register
+              </button>
+            </li>
+            <li className="md:hidden block hover:text-gray-500">Login</li>
+          </ul>
+        </div>
 
-      <div className="flex items-center gap-6">
-        <button className="bg-[#a6c1ee] text-white px-5 py-2 rounded-full hover:bg-[#87acec] mr-[10px]">
-          Register
-        </button>
-        <button className="bg-[#a6c1ee] text-white px-5 py-2 rounded-full hover:bg-[#87acec]">
-          Login
-        </button>
-        <div className="text-3xl cursor-pointer md:hidden" onClick={toggleMenu}>
-          {menuOpen ? <IoIosClose /> : <IoIosMenu />}
-        </div>{' '}
-      </div>
-    </nav>
+        <div className="flex items-center gap-6">
+          <button className="bg-[#355878] text-white px-5 py-2 rounded-full hover:bg-[#87acec] hidden md:block">
+            Register
+          </button>
+          <button className="bg-[#355878] text-white px-5 py-2 rounded-full hover:bg-[#87acec] hidden md:block">
+            Login
+          </button>
+          <div
+            className="text-3xl cursor-pointer md:hidden"
+            onClick={toggleMenu}
+          >
+            {menuOpen ? <IoIosClose /> : <IoIosMenu />}
+          </div>{' '}
+        </div>
+      </nav>
+    </div>
   )
 }
 
