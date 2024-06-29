9995c6f0795cdb1dd75b087b3c8ea860d08fa0e5
Promie Yutasane <promie.yutasane@gmail.com>
2024-06-29 10:48:22 +1000

    CSP-23 fixed navbar

--- src/components/nav/Navbar.tsx
+++ src/components/nav/Navbar.tsx
@@ -21,7 +21,7 @@ const Navbar: FC = () => {
         </div>
 
         <div
-          className={`nav-links duration-500 md:static absolute bg-white md:min-h-fit min-h-[35vh] left-0 ${menuOpen ? 'opacity-100 visible top-[120%]' : 'opacity-0 invisible top-[120%]'} md:opacity-100 md:visible md:top-auto md:flex md:items-center md:px-0 z-50 transition-all md:w-auto w-full`}
+          className={`nav-links duration-500 md:static absolute bg-white md:min-h-fit min-h-[35vh] left-0 ${menuOpen ? 'opacity-100 visible top-[100%]' : 'opacity-0 invisible top-[100%]'} md:opacity-100 md:visible md:top-auto md:flex md:items-center md:px-0 z-50 transition-all md:w-auto w-full`}
         >
           <ul className="flex md:flex-row flex-col md:items-center md:gap-[4vw] gap-8 md:p-0 p-[15px]">
             <li className="hover:text-gray-500 cursor-pointer">Home</li>
